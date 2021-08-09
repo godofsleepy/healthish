81532b8a95b4e0c0b4034a678371da1e244ebb73
Rifat Khadafy <45101689+godofsleepy@users.noreply.github.com>
2021-08-09 16:56:31 +0700

    update splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:healthish/screen/splash_screen/splash.dart';
+import 'package:healthish/screen/splash/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: SplashScreen(),
     );
   }
 }
